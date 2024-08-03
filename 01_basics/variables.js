84a23692a1299121b527c73b0a42436e177c50d8
Shivam Pandey <shivampandey4597@gmail.com>
2024-08-03 11:28:42 +0530

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,24 @@
+const accountId = 10025;
+
+let accountEmail = 'gun1256@gmail.com';
+
+var accountPass = '12345678';
+
+let accountState;
+
+accountCity = 'Delhi';
+
+// accountId = 25569; assignment to constant variable not allowed
+
+// console.log(accountId);
+
+accountEmail = "abc@abc.com";
+accountPass = "1234";
+accountCity = "Lucknow";
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
+
+/**
+ * prefer not to use var 
+ * because of issue in functional and block scope
+ */
