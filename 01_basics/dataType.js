9e6764a5fac3122f23b85695f3c223b54a4401ef
Shivam Pandey <shivampandey4597@gmail.com>
2024-08-03 11:55:47 +0530

    data_types

---
+++ 01_basics/dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as new js version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Shivam"
+let age = 30
+let isLoggedIn = false
+
+// Primitive Data Types
+
+// number => 2^53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// Object
+
+console.log(typeof age); // => number
+console.log(typeof null); // => object
+console.log(typeof undefined); // => undefined
